df3a25c74750fdd675a5089264504d1e3c9ccb90
Jason Webb <bigjasonwebb@gmail.com>
2011-10-31 07:38:58 -0600

    More clarity to how static properties behave

--- spec/object_spec.js
+++ spec/object_spec.js
@@ -7,6 +7,10 @@ describe("Object", function() {
         first_name: "Bob",
         last_name: "Wiley",
         full_name: function() { return this.first_name + " " + this.last_name }
+    }, {
+        newPerson: function() {
+            return new this();
+        }
     });
 
     it("propagates its extend function", function() {
@@ -23,6 +27,16 @@ describe("Object", function() {
             expect(person.first_name).toBe("Bob");
             expect(person.last_name).toBe("Wiley");
             expect(person.full_name()).toBe("Bob Wiley");
+        });
+        it("has static properties", function() {
+            expect(Person.newPerson().first_name).toBe("Bob");
+        });
+        it("creates children without its static props", function() {
+            expect(person.newPerson).toBeUndefined();
+        });
+        it("extends children with its static props", function() {
+            var Child = Person.extend({});
+            expect(Child.newPerson).toBeDefined();
         });
     });
 });
